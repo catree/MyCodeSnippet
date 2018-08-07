2a3b3538e4b329e1832aaf5be3d9f26fb76264e8
AIBluefisher <1701213988@pku.edu.cn>
2018-08-07 19:13:55 +0800

    modify the directory structure

--- ui/public/js/initMap.js
+++
@@ -1,9 +0,0 @@
-function init(){
-    var map = new AMap.Map('container', {
-        center: [113.846118,22.709236],
-        zoom: 16
-    });
-    map.plugin(["AMap.ToolBar"], function() {
-        map.addControl(new AMap.ToolBar());
-    });
-}
