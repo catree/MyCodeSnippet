6a6e11bb251601b04e52a50bd4f073145cbef891
陈煜 <1701213988@pku.edu.cn>
2018-10-20 16:57:44 +0800

    Create CMakeModule.sh

---
+++ CMakeModule.sh
@@ -0,0 +1,8 @@
+#opengl
+sudo apt-get install build-essential libgl1-mesa-dev
+sudo apt-get install freeglut3-dev
+sudo apt-get install libglew-dev libsdl2-dev libsdl2-image-dev libglm-dev libfreetype6-dev
+sudo apt-get install libglfw3-dev libglfw3
+
+sudo apt-get install libpng16-16
+sudo apt-get install libjpeg-dev
